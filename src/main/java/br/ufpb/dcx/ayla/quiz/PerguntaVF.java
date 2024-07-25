e87fd34115089cd398353c8c779916df207aa8c9
alexbrasil12 <alexander.brasil@dcx.ufpb.br>
2024-07-25 10:24:19 -0300

    Delete src/main/java/br/ufpb/dcx/ayla/quiz directory

--- src/main/java/br/ufpb/dcx/ayla/quiz/PerguntaVF.java
+++
@@ -1,9 +0,0 @@
-package br.ufpb.dcx.ayla.quiz;
-
-public class PerguntaVF extends Pergunta {
-    @Override
-    public boolean estahCorretaResposta(String resposta) {
-       //TODO: IMPLEMENTAR
-        return false;
-    }
-}
